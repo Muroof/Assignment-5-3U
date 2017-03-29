998a91e9c2e2dd59c418c65704f889c1b47d32fb
ranam3235 <ranam3235@HRH-IC0027321.wrdsb.ca>
2017-03-29 09:21:12 -0400

    A5

---
+++ src/A5Q1.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ranam3235
+ */
+public class A5Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // scanner to get user input
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("Enter a word:");
+        String word = input.nextLine();
+        
+        
+        
+        
+        
+        
+    }
+}
