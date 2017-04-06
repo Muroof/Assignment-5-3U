b04fdd237a2ab2bc1aafd16002cb3c8bef5fb51e
ranam3235 <ranam3235@HRH-IC0027321.wrdsb.ca>
2017-04-06 10:13:42 -0400

    STILL WORKING ON ASSIGNMENT 5 AHHHHHHHHH

--- src/A5Q1.java
+++ src/A5Q1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ranam3235
@@ -18,12 +17,12 @@ public class A5Q1 {
     public static void main(String[] args) {
         // scanner to get user input
         Scanner input = new Scanner(System.in);
-        
+
         // Ask player for a word
         System.out.println("Enter a word:");
         // Word input
         String word = input.nextLine();
-        
+
         // Spaces to shield word from second player
         System.out.println("");
         System.out.println("");
@@ -41,56 +40,75 @@ public class A5Q1 {
         System.out.println("");
         System.out.println("");
         System.out.println("");
-        
+
         // creating a integer for amount of letters in the word
         int length = word.length();
         // using the integer to display correct amount of underscores
         String underscore = "_";
-        for (int a = 1; a < length; a++){
+        for (int a = 1; a < length; a++) {
             underscore = underscore + " _";
+
+
         }
-         
+
         // prints out underscores
         System.out.println(underscore);
-        
-        
+
+
         // gives the player 5 guesses
         int correct = length;
-        int guesses = 5;
-        
+        int lives = 5;
+
         // loop while they have guesses 
-        while (correct > 0 && guesses > 0) {
-            
-        // asking the player for their guess
-        System.out.println("Player Two: Enter in your guess, you have " + guesses +  " guesses left.");
-        // store their guess and store as a char
-        String guess1 = input.nextLine();
-        char letter1 = guess1.charAt(0);
-        // space
-        System.out.println("");
-        
-        
-        // creates a intger for first letter in guess
-        int letterPos = word.indexOf(letter1);
-            
-        if (letterPos >= 0) {
-            
+        while (correct > 0 && lives > 0) {
+
+            // asking the player for their guess
+            System.out.println("Player Two: Enter in your guess, you have " + lives + " lives left.");
+            // store their guess and store as a char
+            String guess1 = input.nextLine();
+            char letter1 = guess1.charAt(0);
+            // space
+            System.out.println("");
+
+
+
+            // stores letter1 as an integer 
+            int letter1Position = word.indexOf(letter1);
+            // greater than 0 means the letter is in the word
+            if (letter1Position >= 0) {
+
+                while (letter1Position >= 0) {
+
+                    String firstHalf = underscore.substring(0, letter1Position);
+
+                    String lastHalf = underscore.substring(letter1Position + 1, length);
+
+                    underscore = firstHalf + letter1 + lastHalf;
+
+
+
+
+                }
+            }
+
+
+
+
+
+
+
+
         }
-        
-        
-        
-        
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
+
     }
 }
